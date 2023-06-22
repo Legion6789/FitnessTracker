837a56bc4b839b62cf773253f9b64a301a083617
Legion Rempel <legion.rempel@comptrak.com>
2023-06-22 15:15:26 -0400

    Updated to .NET 7.0

---
+++ BusinessEntities/EF/ApiUser.cs
@@ -0,0 +1,10 @@
+﻿//using Microsoft.AspNetCore.Identity;
+
+namespace BusinessEntities.EF
+{
+    //public class ApiUser : IdentityUser
+    //{
+    //    public string FirstName { get; set; }
+    //    public string LastName { get; set; }
+    //}
+}
